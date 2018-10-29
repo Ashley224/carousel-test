38afe8a0f85e0961639784d74077e1d5b0fd20b5
Ashley224 <ashishagrawal224@gmail.com>
2018-10-29 14:27:20 +0530

    Fetch Image Api

---
+++ src/core/constants.js
@@ -0,0 +1,3 @@
+export default {
+	apiUrl: 'https://pixabay.com/api/'
+}
--- src/core/services/ImageFetchService.js
+++ src/core/services/ImageFetchService.js
@@ -1,6 +1,20 @@
+import Constants from '../constants';
+import axios from 'axios';
 export default class ImagesFetchService {
 	constructor() {
-		
+		if(!process.env.REACT_APP_ImageFetch_ApiKey)
+			throw new Error('ApiKey Envrionment Variable Not set before running the Application');	
+		console.log(process.env);
+		this.apiKey = process.env.REACT_APP_ImageFetch_ApiKey;
+		this.apiUrl = Constants.apiUrl;
 	}
-	
+	getData() {
+		const type = 'photo';
+		try {
+			return axios.get(`${this.apiUrl}/?key=${this.apiKey}&q=beautiful+landscape&image_type=${type}`);
+		}
+		catch (err) {
+			throw new Error(err.message);
+		}
+	}
 }
